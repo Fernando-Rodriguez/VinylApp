37842cc320d8fae03acda8a18a924072fdbe980f
Fernando-Rodriguez <62259661+Fernando-Rodriguez@users.noreply.github.com>
2021-09-16 21:48:06 -0400

    cleaning domain.

--- VinylApp.Domain/Models/VinylAppModels/AlbumAggregate/AlbumItem.cs
+++ VinylApp.Domain/Models/VinylAppModels/AlbumAggregate/AlbumItem.cs
@@ -10,5 +10,20 @@ namespace VinylApp.Domain.Models.VinylAppModels.AlbumAggregate
         public string AlbumName { get; set; }
         public string ArtistName { get; set; }
         public string AlbumArtworkUrl { get; set; }
+
+        public void UpdateAlbumName(string name)
+        {
+            AlbumName = name;
+        }
+
+        public void UpdateArtistName(string name)
+        {
+            ArtistName = name;
+        }
+
+        public void UpdateArtworkUrl(string url)
+        {
+            AlbumArtworkUrl = url;
+        }
     }
 }
